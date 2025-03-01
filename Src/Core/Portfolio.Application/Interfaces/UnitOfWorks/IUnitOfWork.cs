dd624490fd8e0da34744242e1c6aef182b7d4ef7
Your Name <you@example.com>
2025-03-01 14:25:22 +0400

    CreatedIUnitOfWork

---
+++ Src/Core/Portfolio.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Portfolio.Application.Interfaces.Repositories;
+using Portfolio.Domain.Entities.Common;
+
+namespace Portfolio.Application.Interfaces.UnitOfWorks;
+
+public interface IUnitOfWork:IDisposable
+{
+    public IWriteRepository<TEntity> GetWriteRepository<TEntity>() where TEntity : class, IBaseEntity, new();
+    public IReadRepository<TEntity> GetReadRepository<TEntity>() where TEntity:class,IBaseEntity,new();
+    public Task<int> SaveAsync();
+    public int Save();
+}

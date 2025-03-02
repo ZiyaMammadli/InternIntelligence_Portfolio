a3ecd33e73cc14b9115a64673fb565c90df67204
Your Name <you@example.com>
2025-03-02 22:52:59 +0400

    CreatedBaseException

---
+++ Src/Core/Portfolio.Application/Exceptions/Base/BaseException.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Application.Exceptions.Base;
+
+public class BaseException:Exception
+{
+    public int StatusCode { get; set; }
+    public BaseException() { }
+    public BaseException(string message) : base(message) { }
+    public BaseException(int statusCode,string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}

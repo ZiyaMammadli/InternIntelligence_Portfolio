d84c025392428de4d5ee226db0eeab11ef0d50a6
Your Name <you@example.com>
2025-03-03 17:12:34 +0400

    CompletedLoginController

---
+++ Src/Presentation/Portfolio.Api/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Application.Features.Auth.Commands.Login;
+
+namespace Portfolio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<IActionResult> Login(LoginCommandRequest request)
+        {
+            var response=await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}

e64b310b80bb952af33ead7194cf494f983900ac
Your Name <you@example.com>
2025-03-06 11:57:11 +0400

    UpdatedProjectValidations

--- Src/Core/Portfolio.Application/Features/Projects/Commands/Create/CreateProjectCommandValidator.cs
+++ Src/Core/Portfolio.Application/Features/Projects/Commands/Create/CreateProjectCommandValidator.cs
@@ -11,12 +11,15 @@ public class CreateProjectCommandValidator : AbstractValidator<CreateProjectComm
             .NotNull().WithMessage("UserId can not be null");
         RuleFor(p => p.Name)
             .NotEmpty().WithMessage("Name can not be Empty")
-            .NotNull().WithMessage("Name can not be null");
+            .NotNull().WithMessage("Name can not be null")
+            .MaximumLength(20).WithMessage("Maximum Lentgh must be 20");
         RuleFor(p => p.Description)
             .NotEmpty().WithMessage("Description can not be Empty")
-            .NotNull().WithMessage("Description can not be null");
+            .NotNull().WithMessage("Description can not be null")
+            .MaximumLength(100).WithMessage("Maximum Lentgh must be 100");
         RuleFor(p => p.Link)
             .NotEmpty().WithMessage("Link can not be Empty")
-            .NotNull().WithMessage("Link can not be null");
+            .NotNull().WithMessage("Link can not be null")
+            .MaximumLength(250).WithMessage("Maximum Lentgh must be 250");
     }
 }
